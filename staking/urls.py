0523939fadea665dda3d73afe6ff96825608efda
ray <temi@temis-MacBook-Pro.local>
2023-07-02 14:21:57 +0100

    ido app deployed

--- staking/urls.py
+++ staking/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "stake"
 
 urlpatterns = [
-	path("pools/", views.IndexView, name="pool"),
-	path("stake/", views.StakeView, name="stake"),
-	path("harvest/", views.HarvestView, name="harvest"),
+	path("pools/<int:ido_id>/", views.IndexView, name="pool"),
+	path("stake/<int:ido_id>/<int:amount>/", views.StakeView, name="stake"),
+    path("my-stakes/", views.MyStakesView, name="my_stakes"),
+	path("harvest/<int:stake_id>/", views.HarvestView, name="harvest"),
 ]
